57a0deb61d5e572ca5f2e6272465d4905aa298f3
Gilmar J. A. Goulart <gilmar.g@outlook.com>
2017-08-22 16:33:32 -0300

    Ajustado leitura do arquivo com a Matriz de Adjacência.

--- src/mainProgram/MainProgram.java
+++ src/mainProgram/MainProgram.java
@@ -1,8 +1,8 @@
 package mainProgram;
 
 import java.io.FileNotFoundException;
-
-import com.google.gson.Gson;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import utilitarios.TextReader;
 
@@ -19,12 +19,26 @@ import utilitarios.TextReader;
  */
 public class MainProgram {
 	
-	public static String CONFIG_FILE_PATH = "matriz.json"; 
+	private static String CONFIG_FILE_PATH = "matriz.txt"; 
+	private static String START_POINT = "A", END_POINT = "D"; 
 	
 	public static void main(String[] args) {
+		//
+		//Exibir o que veio por parâmetro.
+		int i = 0;
+		for (String s : args) {
+			System.out.println("args[" + i + "] = " + s);
+			i++;
+		}
+		//
+		//
 		
-		//Parâmetro da Matriz de Adjacência
-		CONFIG_FILE_PATH = args[0];
+		if (args.length > 0) {
+			//Parâmetro da Matriz de Adjacência
+			CONFIG_FILE_PATH	= args[0]; // Primeiro Parâmetro é o caminho do arquivo.
+			START_POINT			= args[1]; // Segundo parâmetro é o ponto de origem.
+			END_POINT			= args[2]; // Terceiro parâmetro é o ponto de destino.
+		}
 		
 		/*
 		“Sobre a entrada <G,s,t> onde G é um grafo direcionado com nos S e T:
@@ -33,28 +47,49 @@ public class MainProgram {
 		3. Faça uma varredura em todas as arestas de G. Se uma aresta (a,b) for encontrada indo de um nó marcado A para um nó não marcado B, marque o nó B.
 		4. Se T estiver marcado, aceite. Caso contrário, rejeite.”
 		*/
-		
-		Gson g = new Gson();
-		
-		//Ler arquivo de texto, com a configuração armazenada em Json
-		String txtJson = null;
+
 		try {
-			txtJson = TextReader.readTextFile(CONFIG_FILE_PATH);
+			//Ler arquivo de texto, com a Matriz de adjacência.
+			String txtJson = TextReader.readTextFile(CONFIG_FILE_PATH).replaceAll(" ", "");
 			
-			// TODO Instanciar classe de configuração com a matriz
-			//configuracao = g.fromJson(txtJson, Configuracao.class);
-		
+			//Quebra cada linha em um vetor
+			String [] txtVetor = txtJson.split("\n");
+			
+			//Primeira linha contém os cabeçalhos, assim como cada linha na mesma posição
+			char [] cabecalhos = txtVetor[0].trim().toCharArray();
+			
+			//Inicializa a Matriz
+			String [][] txtMatriz = new String[cabecalhos.length][cabecalhos.length];
+			int linha = 0, coluna = 0;
+			
+			//Começar contagem em 1, pois 0 são os cabeçalhos
+			for (int k = 1; k < txtVetor.length; k++) {
+				
+				//Remover quebras de linha
+				String c = txtVetor[k].replaceAll("\\n|\\r", "");
+				
+				coluna = 0;
+				//Alimentar a Matriz em cada posição
+				for (int j = 1; j < c.length(); j++) {
+					txtMatriz[linha][coluna] = String.valueOf(c.charAt(j));
+					coluna++;
+				}
+				linha++;
+				
+			}
+			
+			
+			for (String[] ss : txtMatriz) {
+				for (String string : ss) {
+					System.out.print(string);
+				}
+				System.out.println();
+			}
+			
 		} catch (FileNotFoundException fe) {
 			fe.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		
-		int i = 0;
-		
-		for (String s : args) {
-			System.out.println("args[" + i + "] = " + s);
-			i++;
 		}
 	}
 }
